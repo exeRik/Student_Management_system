f6bc291b8eb0a8ece5da7eb1d8a02c98a2138f2e
exeRik <smarika.p17@gmail.com>
2025-08-31 18:59:32 +0545

    final few changes

--- src/components/table/ActionButtons.jsx
+++ src/components/table/ActionButtons.jsx
@@ -5,7 +5,7 @@ export default function ActionButtons({ onEdit, onDelete }) {
     <div className="flex gap-2">
       <button
         onClick={onEdit}
-        className="px-3 py-1 rounded bg-yellow-300 hover:bg-yellow-400 text-sm font-medium transition"
+        className="px-3 py-1 rounded bg-blue-900 hover:bg-blue-700 text-sm font-medium  text-white transition"
       >
         Edit
       </button>
@@ -13,7 +13,7 @@ export default function ActionButtons({ onEdit, onDelete }) {
         onClick={() => {
           if (confirm("Delete this student?")) onDelete();
         }}
-        className="px-3 py-1 rounded bg-red-500 hover:bg-red-600 text-sm font-medium text-white transition"
+        className="px-3 py-1 rounded bg-red-900 hover:bg-red-700 text-sm font-medium text-white transition"
       >
         Delete
       </button>
